f1889d2e41f2efd57411efece6a99bfbdeff3883
Jakub Ocwieja <netkuba@students.mimuw.edu.pl>
2012-02-11 20:09:54 +0100

    Current user loading before application start.

--- PengurusCRM/src/main/java/com/pengurus/crm/client/PengurusCRM.java
+++ PengurusCRM/src/main/java/com/pengurus/crm/client/PengurusCRM.java
@@ -2,16 +2,44 @@ package com.pengurus.crm.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.pengurus.crm.client.service.CurrentSessionService;
+import com.pengurus.crm.client.service.CurrentSessionServiceAsync;
+import com.pengurus.crm.shared.dto.UserDTO;
 
 public class PengurusCRM implements EntryPoint {
+	
 	public MainWindow mainWindow;
+	public static UserDTO currentUser;
 	
 	public void onModuleLoad() {
+		startWithCurrentUserLoaded();
+	}
+	
+	private void startWithCurrentUserLoaded() {
+		AsyncCallback<UserDTO> callback = new AsyncCallback<UserDTO>() {
+
+			public void onFailure(Throwable t) {
+				Window.Location.assign("/spring_security_login");
+			}
+
+			public void onSuccess(UserDTO result) {
+				currentUser = result;
+				loadMainWindow();
+			}
+		};
+		CurrentSessionServiceAsync service = (CurrentSessionServiceAsync) GWT
+				.create(CurrentSessionService.class);
+		service.getCurrentUser(callback);
+	}
+
+	private void loadMainWindow() {
 		mainWindow = GWT.create(MainWindow.class);
 		RootPanel.get().add(mainWindow);
 	}
-	
+
 	public MainWindow getMainWindow() {
 		return mainWindow;
 	}
--- PengurusCRM/src/main/java/com/pengurus/crm/client/panel/menu/QuotesMenuPanel.java
+++ PengurusCRM/src/main/java/com/pengurus/crm/client/panel/menu/QuotesMenuPanel.java
@@ -7,6 +7,7 @@ import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.pengurus.crm.client.PengurusCRM;
 import com.pengurus.crm.client.panel.center.QuotePanel;
 import com.pengurus.crm.client.service.CurrentSessionService;
 import com.pengurus.crm.client.service.CurrentSessionServiceAsync;
@@ -19,21 +20,6 @@ public class QuotesMenuPanel extends TabMenuPanel {
 	    panel.setAnimCollapse(false);  
 	    panel.setHeading("QUOTES");
 	    panel.setBodyStyleName("pad-text");  
-	    
-		AsyncCallback<UserDTO> callback = new AsyncCallback<UserDTO>() {
-
-			public void onFailure(Throwable t) {
-				MessageBox.info("Message", t.toString(), null);
-			}
-
-			public void onSuccess(UserDTO result) {
-				MessageBox.info("Message", result.getUsername(), null);
-			}
-		};
-		CurrentSessionServiceAsync service = (CurrentSessionServiceAsync) GWT
-				.create(CurrentSessionService.class);
-		service.getCurrentUser(callback);
-		
 	    Button b = new Button("Last Edited");
 	    b.addSelectionListener(new SelectionListener<ButtonEvent>(){
 			@Override
