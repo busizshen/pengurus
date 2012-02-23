8c33e7eb42eabca0785fdd3b0fd266a10707725f
Jakub Oćwieja <netkuba@students.mimuw.edu.pl>
2012-02-23 13:26:57 +0100

    [CRM-17] Create "Create user" view - mockup.
    Change-Id: Ic770a937f5433c2a0fbee91ec29b97f0ab2d4c54

--- PengurusCRM/src/main/java/com/pengurus/crm/client/panels/center/UserCreatePanel.java
+++ PengurusCRM/src/main/java/com/pengurus/crm/client/panels/center/UserCreatePanel.java
@@ -1,35 +1,48 @@
 package com.pengurus.crm.client.panels.center;
 
+import com.extjs.gxt.ui.client.Style.Orientation;
 import com.extjs.gxt.ui.client.event.BaseEvent;
 import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
-import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.extjs.gxt.ui.client.widget.VerticalPanel;
+import com.extjs.gxt.ui.client.widget.form.CheckBox;
+import com.extjs.gxt.ui.client.widget.form.CheckBoxGroup;
+import com.extjs.gxt.ui.client.widget.form.Field;
 import com.extjs.gxt.ui.client.widget.form.FormPanel;
 import com.extjs.gxt.ui.client.widget.form.FormPanel.LabelAlign;
 import com.extjs.gxt.ui.client.widget.form.Radio;
 import com.extjs.gxt.ui.client.widget.form.RadioGroup;
 import com.extjs.gxt.ui.client.widget.form.TextField;
 import com.extjs.gxt.ui.client.widget.layout.FormData;
+import com.pengurus.crm.shared.dto.UserRoleDTO;
 
 public class UserCreatePanel extends LayoutContainer {
-	
+
 	private VerticalPanel verticalPanel;
 	private FormPanel form;
 	private FormData formData;
 	private TextField<String> username, password, description, fullName;
 	private RadioGroup userType;
-	
+	private CheckBoxGroup userRoles;
+
 	public UserCreatePanel() {
 		createForm();
 		createUsernameField();
 		createPasswordField();
 		createDescriptionField();
 		createUserTypeRatioGroup();
+		createRoleCheckbox();
 		createIndividualClientFullNameField();
-		//createRoleCheckbox();
 		addVerticalPanel();
+	}
+
+	protected RadioGroup getUserType() {
+		return userType;
+	}
+
+	protected void setUserType(RadioGroup userType) {
+		this.userType = userType;
 	}
 
 	private void addVerticalPanel() {
@@ -76,34 +89,59 @@ public class UserCreatePanel extends LayoutContainer {
 				@Override
 				public void handleEvent(BaseEvent be) {
 					for (int i = 0; i < userType.getAll().size(); ++i) {
-						UserTypeRadio radio = (UserTypeRadio)userType.getAll().get(i);
+						UserTypeRadio radio = (UserTypeRadio) userType.getAll()
+								.get(i);
 						radio.onDeselect();
 					}
 					onSelect();
 				}
-				
+
 			});
 		}
+
+		private void setRoleValue(UserRoleDTO userRole, Boolean value) {
+			for (Field<?> field : userRoles.getAll()) {
+				if (field instanceof UserRoleBox) {
+					UserRoleBox userRoleBox = (UserRoleBox) field;
+					if (userRoleBox.getUserRole().equals(userRole)) {
+						userRoleBox.setValue(value);
+					}
+				}
+			}
+		}
+
+		protected void selectRole(UserRoleDTO userRole) {
+			setRoleValue(userRole, true);
+		}
+
+		protected void deselectRole(UserRoleDTO userRole) {
+			setRoleValue(userRole, false);
+		}
+
 		protected abstract void onSelect();
 		protected abstract void onDeselect();
 	}
-	
+
 	private class IndividualClientRadio extends UserTypeRadio {
 		public IndividualClientRadio() {
 			super("Individual client");
 		}
-		
+
 		@Override
 		protected void onSelect() {
+			selectRole(UserRoleDTO.ROLE_USER);
+			selectRole(UserRoleDTO.ROLE_CLIENT);
 			fullName.show();
 		}
 
 		@Override
 		protected void onDeselect() {
+			deselectRole(UserRoleDTO.ROLE_USER);
+			deselectRole(UserRoleDTO.ROLE_CLIENT);
 			fullName.hide();
-		}	
+		}
 	}
-	
+
 	private class BusinessClientRadio extends UserTypeRadio {
 		public BusinessClientRadio() {
 			super("Business client");
@@ -111,13 +149,107 @@ public class UserCreatePanel extends LayoutContainer {
 
 		@Override
 		protected void onSelect() {
-			// TODO Auto-generated method stub
+			selectRole(UserRoleDTO.ROLE_USER);
+			selectRole(UserRoleDTO.ROLE_CLIENT);
 		}
 
 		@Override
 		protected void onDeselect() {
-			// TODO Auto-generated method stub
+			deselectRole(UserRoleDTO.ROLE_USER);
+			deselectRole(UserRoleDTO.ROLE_CLIENT);
 		}
+	}
+
+	private class WorkerRadio extends UserTypeRadio {
+		public WorkerRadio() {
+			super("Worker");
+		}
+
+		@Override
+		protected void onSelect() {
+			selectRole(UserRoleDTO.ROLE_USER);
+
+		}
+
+		@Override
+		protected void onDeselect() {
+			deselectRole(UserRoleDTO.ROLE_USER);
+
+		}
+	}
+
+	private class TranslatorRadio extends UserTypeRadio {
+		public TranslatorRadio() {
+			super("Translator");
+		}
+
+		@Override
+		protected void onSelect() {
+			selectRole(UserRoleDTO.ROLE_USER);
+			selectRole(UserRoleDTO.ROLE_EXPERT);
+
+		}
+
+		@Override
+		protected void onDeselect() {
+			deselectRole(UserRoleDTO.ROLE_USER);
+			deselectRole(UserRoleDTO.ROLE_EXPERT);
+
+		}
+
+	}
+
+	private void createUserTypeRatioGroup() {
+		userType = new RadioGroup("Usertype");
+		userType.setFieldLabel("User type");
+		userType.setOrientation(Orientation.VERTICAL);
+		userType.add(new IndividualClientRadio());
+		userType.add(new BusinessClientRadio());
+		userType.add(new WorkerRadio());
+		userType.add(new TranslatorRadio());
+		form.add(userType, formData);
+	}
+
+	private class UserRoleBox extends CheckBox {
+		private UserRoleDTO userRole;
+
+		public UserRoleBox(String label, UserRoleDTO userRole, Boolean enabled) {
+			super();
+			setBoxLabel(label);
+			this.userRole = userRole;
+			this.setEnabled(enabled);
+		}
+
+		public UserRoleBox(String label, UserRoleDTO userRole) {
+			this(label, userRole, true);
+		}
+
+		public UserRoleDTO getUserRole() {
+			return userRole;
+		}
+	}
+
+	private void createRoleCheckbox() {
+		userRoles = new CheckBoxGroup();
+		userRoles.setFieldLabel("User roles");
+		userRoles.setOrientation(Orientation.VERTICAL);
+		userRoles
+				.add(new UserRoleBox("Role user", UserRoleDTO.ROLE_USER, false));
+		userRoles.add(new UserRoleBox("Role client", UserRoleDTO.ROLE_CLIENT,
+				false));
+		userRoles.add(new UserRoleBox("Role expert", UserRoleDTO.ROLE_EXPERT,
+				false));
+		userRoles.add(new UserRoleBox("Role accountant",
+				UserRoleDTO.ROLE_ACCOUNTANT));
+		userRoles.add(new UserRoleBox("Role executive",
+				UserRoleDTO.ROLE_EXECUTIVE));
+		userRoles.add(new UserRoleBox("Role freelancer",
+				UserRoleDTO.ROLE_FREELANCER));
+		userRoles.add(new UserRoleBox("Role project manager",
+				UserRoleDTO.ROLE_PROJECTMNAGER));
+		userRoles.add(new UserRoleBox("Role verificator",
+				UserRoleDTO.ROLE_VERIFICATOR));
+		form.add(userRoles);
 	}
 
 	private void createIndividualClientFullNameField() {
@@ -126,14 +258,4 @@ public class UserCreatePanel extends LayoutContainer {
 		fullName.hide();
 		form.add(fullName, formData);
 	}
-	
-	private void createUserTypeRatioGroup() {
-		userType = new RadioGroup("Usertype");
-		userType.setFieldLabel("User type");
-		userType.add(new IndividualClientRadio());
-		userType.add(new BusinessClientRadio());
-		form.add(userType, formData);
-	}
-
-	
 }
