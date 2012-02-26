32b70f472eff198b666a4d65a98e245dbaeeb188
Krzysiek Gogolewski <krzysiek2605@gmail.com>
2012-02-26 22:46:01 +0100

    [CRM-20] Filepath added.
    Change-Id: I4c8e9b57dab319df0422d6550d9052f5d121830c

--- PengurusCRM/restore.sh
+++ PengurusCRM/restore.sh
@@ -1,3 +1,3 @@
 sudo -u postgres psql --command='drop schema public cascade' pengurus postgres
 sudo -u postgres psql --command='create schema public' pengurus postgres
-sudo -u postgres psql --file create.sql pengurus postgres
+sudo -u postgres psql --file `dirname $0`/create.sql pengurus postgres
