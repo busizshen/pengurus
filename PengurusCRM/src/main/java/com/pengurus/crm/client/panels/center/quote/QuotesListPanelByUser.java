af7707ea63988c1246520c622a33cf376e667819
Jakub Oćwieja <netkuba@students.mimuw.edu.pl>
2012-04-18 21:14:33 +0200

    [minor] Quote pagination quick fix.
    Change-Id: Ib3551177d7ffd9d24352354c250984409ac81fdc

--- PengurusCRM/src/main/java/com/pengurus/crm/client/panels/center/quote/QuotesListPanelByUser.java
+++ PengurusCRM/src/main/java/com/pengurus/crm/client/panels/center/quote/QuotesListPanelByUser.java
@@ -7,12 +7,14 @@ public abstract class QuotesListPanelByUser extends QuotesListPanel {
 	protected UserDTO userDTO;
 
 	public QuotesListPanelByUser(UserDTO userDTO) {
+		initPagination();
 		this.userDTO = userDTO;
 		modelList = new ModelList();
 		add(modelList);
 	}
 
 	public QuotesListPanelByUser(UserDTO userDTO, int height, int width) {
+		initPagination();
 		this.userDTO = userDTO;
 		modelList = new ModelList(height, width);
 		add(modelList);
